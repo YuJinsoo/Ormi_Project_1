80049943411b40412cb8f7127eba35750bb62563
YuJinsoo <realaqua23@gmail.com>
2023-06-18 18:55:06 +0900

    api get호출시 cors 에러 해결

--- src/wordSearchAPI.js
+++ src/wordSearchAPI.js
@@ -1,26 +1,23 @@
 //
 
 const url = "https://stdict.korean.go.kr/api/search.do";
-const testurl =
-  "https://stdict.korean.go.kr/api/search.do?certkey_no=5495&key=DFCABB03AB1D381AE4FF292B620877EE&type_search=search&req_type=json&q=%EB%82%98%EB%AC%B4";
+
 export function koWordAPI(word) {
-  let search = `${url}?key=DFCABB03AB1D381AE4FF292B620877EE&type_search=search&q=${word}`;
-  fetch(search, {
+  fetch(url, {
     method: "GET",
+    certkey_no: 5518,
+    key: "5506921242C21452811AFA56D1F94A04",
+    q: `${word}`,
+    encoding: "utf-8",
+    // type_search: "search",
+    // req_type: "json",
+    mode: "no-cors",
     headers: {
       "Content-Type": "application/json",
     },
 
     redirect: "follow",
-  }).then((res) => console.log(res));
+  })
+    //   .then((res) => res.json());
+    .then((res) => console.log(res));
 }
-
-// body: JSON.stringify({
-//     key: "DFCABB03AB1D381AE4FF292B620877EE",
-//     q: word,
-//     req_type: "json",
-//     advanced: "y",
-//     method: "exact",
-//     type1: "word",
-//     pos: [1, 2],
-//   }),
