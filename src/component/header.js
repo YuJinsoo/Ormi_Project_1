2ca5dfe3e6ff675c60a6dcef68b19f6c7db41b2c
YuJinsoo <realaqua23@gmail.com>
2023-07-14 17:23:09 +0900

    햄버거 메뉴 추가 및 스타일 개발 완료. 메뉴리스트 generator 로 적용 완료.

--- src/component/header.js
+++ src/component/header.js
@@ -7,6 +7,9 @@ import { menuGenerator } from "./menuGenerator.js";
 export function headerGenerator() {
   let resultNode = createNode("header", "navbar");
 
+  let head = createNode("div", "head");
+  let logoDiv = createNode("div", "logodiv");
+
   let logo = createNode("a", "logolink");
   logo.href = "./index.html";
 
@@ -17,83 +20,25 @@ export function headerGenerator() {
   let logoTitle = createNode("div", null, "titlelogo");
   logoTitle.innerText = "VERSUS GPT";
   logo.append(logoTitle);
+  logoDiv.append(logo);
 
-//   let language = createNode("div", "language");
+  head.append(logoDiv);
 
-//   let langText = createNode("p", null, "titlelogo");
-//   langText.textContent = "Language:";
-//   language.append(langText);
+  let toggleDiv = createNode("div", "togglediv");
 
-//   //
-//   let langSelbox = createNode("select", "langselect");
+  let toggleMenu = createNode("a", "toggleMenu");
+  toggleMenu.href = "#"
+  let menulogo = createNode("img", "logomenu");
+  menulogo.src = "./asset/menu.svg";
+  toggleMenu.append(menulogo);
+  toggleDiv.append(toggleMenu)
 
-//   let optKo = createNode("option");
-//   optKo.innerText = "Korean";
-//   langSelbox.append(optKo);
-
-//   let optEn = createNode("option");
-//   optEn.innerText = "English";
-//   langSelbox.append(optEn);
-//   language.append(langSelbox);
-
-  resultNode.append(logo);
-//   resultNode.append(language);
-
-  // menu label 버튼
-//   let menu = createNode("label", "menu");
-//   let menuImg = createNode("img");
-//   menuImg.src = "./asset/menu.svg";
-//   menu.append(menuImg);
-//   let menuInput = createNode("input", "menucheck");
-//   menuInput.type = "checkbox";
-//   menu.append(menuInput);
-//   menu.for = "menucheck";
-//   resultNode.append(menu);
-
-//   let menuwindow = menuGenerator();
-//   resultNode.append(menuwindow);
+  head.append(toggleDiv);
+  resultNode.append(head);
 
 
-  // 반응형 헤더 메뉴
-  let menulist = createNode("ul", "navbar_menu");
-
-  let li_1 = createNode("li", null, "menu_style")
-  let a1 = createNode("a");
-  a1.href = "./index.html";
-  a1.innerText = "Home";
-  li_1.append(a1);
-  menulist.append(li_1);
-
-  let li_2 = createNode("li", null, "menu_style")
-  li_2.innerText = "Language"
-  let selbox = createNode("select", "langselect");
-  let opt_ko = createNode("option");
-  opt_ko.innerText = "Korean";
-  selbox.append(opt_ko);
-
-  let opt_en = createNode("option");
-  opt_en.innerText = "English";
-  selbox.append(opt_en);
-  li_2.append(selbox);
-  menulist.append(li_2);
-
-  let li_3 = createNode("li", null, "menu_style")
-  let b1 = createNode("button", "modeButton");
-  b1.innerText = "Mode";
-  let modeinput = createNode("input", "modeCheck");
-  modeinput.type = "checkbox";
-  b1.for = "modeCheck";
-  li_3.append(b1, modeinput);
-  menulist.append(li_3);
-
-  let li_4 = createNode("li", null, "menu_style")
-  let a2 = createNode("a");
-  a2.href = "https://github.com/YuJinsoo/Ormi_Project_1";
-  a2.target = "_blank";
-  a2.innerText = "Github Repo";
-  li_4.append(a2);
-  menulist.append(li_4);
-  
+  // 반응형 헤더 메뉴 생성
+  let menulist = menuGenerator();
   resultNode.append(menulist);
 
   return resultNode;
--- src/component/menuGenerator.js
+++ src/component/menuGenerator.js
@@ -1,47 +1,44 @@
 import { createNode } from "./createNode.js";
 
 export function menuGenerator() {
-  let resultNode = createNode("div", "menuMain");
+  let resultNode = createNode("ul", "navbar_menu");
 
-  let list = createNode("ul");
-  let m1 = createNode("li");
+  let li_1 = createNode("li", null, "menu_style")
   let a1 = createNode("a");
   a1.href = "./index.html";
-  a1.innerText = "home";
+  a1.innerText = "Home";
+  li_1.append(a1);
+  resultNode.append(li_1);
 
-  m1.append(a1);
-  list.append(m1);
+  let li_2 = createNode("li", null, "menu_style")
+  li_2.innerText = "Language"
+  let selbox = createNode("select", "langselect");
+  let opt_ko = createNode("option");
+  opt_ko.innerText = "Korean";
+  selbox.append(opt_ko);
 
-  let m2 = createNode("li");
-  let mode = createNode("label", "modeLabel");
-  mode.innerText = "dark mode on/off";
+  let opt_en = createNode("option");
+  opt_en.innerText = "English";
+  selbox.append(opt_en);
+  li_2.append(selbox);
+  resultNode.append(li_2);
 
+  let li_3 = createNode("li", null, "menu_style")
+  let b1 = createNode("button", "modeButton");
+  b1.innerText = "Mode";
   let modeinput = createNode("input", "modeCheck");
   modeinput.type = "checkbox";
+  b1.for = "modeCheck";
+  li_3.append(b1, modeinput);
+  resultNode.append(li_3);
 
-  mode.for = "modeCheck";
-  mode.append(modeinput);
-  m2.append(mode);
+  let li_4 = createNode("li", null, "menu_style")
+  let a2 = createNode("a");
+  a2.href = "https://github.com/YuJinsoo/Ormi_Project_1";
+  a2.target = "_blank";
+  a2.innerText = "Github Repo";
+  li_4.append(a2);
+  resultNode.append(li_4);
 
-  list.append(m2);
-
-  //   let m3 = createNode("li");
-  //   let a3 = createNode("a");
-  //   a3.href = "";
-  //   a3.innerText = "mode";
-
-  //   m3.append(a3);
-  //   list.append(m3);
-
-  let m4 = createNode("li");
-  let a4 = createNode("a");
-  a4.href = "https://github.com/YuJinsoo/Ormi_Project_1";
-  a4.target = "_blank";
-  a4.innerText = "github repo";
-
-  m4.append(a4);
-  list.append(m4);
-
-  resultNode.append(list);
   return resultNode;
 }
